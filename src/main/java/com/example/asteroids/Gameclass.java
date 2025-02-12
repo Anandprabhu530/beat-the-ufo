e4ff9e9b89bc42944f7c58bc7f9d696778b7b100
Anandprabhu530 <anandprabhu530@gmail.com>
2025-02-12 21:16:36 +0530

    Temp pause on this. Changing works

--- src/main/java/com/example/asteroids/Gameclass.java
+++ src/main/java/com/example/asteroids/Gameclass.java
@@ -30,47 +30,60 @@ public class Gameclass extends JPanel implements Runnable{
 	
 	@Override
 	public void run() {
-		double currTime = 1000000000/FPS;
-		double timeDiff = System.nanoTime()+currTime;
+//		double currTime = 1000000000/FPS;
+//		double timeDiff = System.nanoTime()+currTime;
 		while(gameThread!=null) {
 			// Update game
 			update();
 			// Render to screen
 			repaint();
-			try {
-				double remTime = timeDiff - System.nanoTime();
-				remTime = remTime/1000000;
-				
-				if(remTime<0) {  
-					remTime = 0;
-				}
-				
-				Thread.sleep((long)remTime);
-				
-				timeDiff += currTime;
-			}catch(Exception e){
-				System.out.println("Error" + e);
-			}
+//			try {
+//				double remTime = timeDiff - System.nanoTime();
+//				remTime = remTime/1000000;
+//				
+//				if(remTime<0) {  
+//					remTime = 0;
+//				}
+//				
+//				Thread.sleep((long)remTime);
+//				
+//				timeDiff += currTime;
+//			}catch(Exception e){
+//				System.out.println("Error" + e);
+//			}
 		}
 	}
 	
 	public void update() {
-		if(keyHandler.keyUp) {
-			YOffset -= dif;
-		}else if(keyHandler.keyDown) {
-			YOffset += dif;
-		}else if(keyHandler.keyLeft) {
-			XOffset -= dif;
-		}else if(keyHandler.keyRight) {
-			XOffset += dif;
-		}
+//		if(keyHandler.keyUp) {
+//			YOffset -= dif;
+//		}else if(keyHandler.keyDown) {
+//			YOffset += dif;
+//		}else if(keyHandler.keyLeft) {	
+//			XOffset -= dif;
+//		}else if(keyHandler.keyRight) {
+//			XOffset += dif;
+//		}
 	}
 	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Graphics2D graphics = (Graphics2D)g;
 		graphics.setColor(Color.white);
-		graphics.fillRect(XOffset, YOffset, 15, 15);
+		graphics.drawPolygon(new int[] {10, 20, 30}, new int[] {10, 30, 10}, 3);
+		
+	
+		// 10, 10
+		// 20, 30
+		// 30, 10
+		 	
+		// (10,10)*------*(30,10)
+//		           \    /
+//		            \  /
+//		             \/
+//		              *(20,30)
+		
+//		graphics.rotate(30, XOffset, YOffset);;
 		graphics.dispose();
 	}
 }
